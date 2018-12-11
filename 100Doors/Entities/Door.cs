8ffe1c904f54e1aa3e06ff2f0adb795133ca41c9
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2018-12-11 14:09:10 -0600

    Completed basic solution.

---
+++ 100Doors/Entities/Door.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _100Doors.Entities
+{
+    class Door
+    {
+        public Door(int doorNumber)
+        {
+            Open = false;
+            DoorNumber = doorNumber;
+        } 
+
+        public bool Open { get; set; }
+        public int DoorNumber { get; private set; }
+
+        public bool ToggleOpen()
+        {
+            Open = !Open;
+            return Open;
+        }
+    }
+}
---
+++ 100Doors/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using _100Doors.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _100Doors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int NUMBER_OF_DOORS = 100;
+            var doors = new List<Door>();
+            var sb = new StringBuilder();
+
+            //create collection of doors
+            for (var i = 0; i < NUMBER_OF_DOORS; i++)
+            {
+                doors.Add(new Door(i + 1));
+            }
+
+            //outer loop, for each iterator
+            for (var i = 0; i < NUMBER_OF_DOORS; i++)
+            {
+                Iterate(i + 1, ref doors);
+            }
+
+            var openDoors = doors.Where(o => o.Open).ToList();
+
+            sb.AppendLine($"Number of open doors: {openDoors.Count}");
+
+            foreach (var openDoor in openDoors)
+            {
+                sb.Append($"{openDoor.DoorNumber}, ");
+            }
+
+            Console.Write(sb.ToString());
+            Console.ReadKey();
+        }
+
+        private static void Iterate(int modulus, ref List<Door> doors)
+        {
+            var index = 1;
+            foreach (var door in doors)
+            {
+                if (index % modulus == 0)
+                {
+                    door.ToggleOpen();
+                }
+
+                index++;
+            }
+        }
+    }
+}
